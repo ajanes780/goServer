780aaa0b1d37f757f8aff1e09f08c5ce31e8921d
Aaron Janes <aaron.janes@gmail.com>
2023-06-27 22:50:56 -0600

    added map and struts for route

--- wiki.go
+++ wiki.go
@@ -12,19 +12,10 @@ type Page struct {
 	Body  []byte
 }
 
-type router struct {
-	path    string
-	handler func(w http.ResponseWriter, r *http.Request)
-}
-
-var viewRoute = router{path: "/view/", handler: viewHandler}
-var editRoute = router{path: "/edit/", handler: editHandler}
-
-// var saveRoute = router{path: "/save/", handler: saveHandler}
-var routes = map[string]router{
-	"view": viewRoute,
-	"edit": editRoute,
-	//"save": editRoute,
+var routes = map[string]string{
+	"view": "/view/",
+	"edit": "/edit/",
+	"save": "/save/",
 }
 
 func (p *Page) save() error {
@@ -48,17 +39,17 @@ func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
 }
 
 func viewHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len(routes["view"].path):]
+	title := r.URL.Path[len(routes["view"]):]
 	p, err := loadPage(title)
 
 	if err != nil {
-		http.Redirect(w, r, "/edit/"+title, http.StatusNotFound)
+		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
 	}
 	renderTemplate(w, "view", p)
 }
 
 func editHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len(routes["edit"].path):]
+	title := r.URL.Path[len(routes["edit"]):]
 	p, err := loadPage(title)
 	if err != nil {
 		p = &Page{Title: title}
@@ -67,10 +58,18 @@ func editHandler(w http.ResponseWriter, r *http.Request) {
 	renderTemplate(w, "edit", p)
 }
 
+func saveHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len(routes["save"]):]
+	body := r.FormValue("body")
+	p := &Page{Title: title, Body: []byte(body)}
+	p.save()
+	http.Redirect(w, r, "/view/"+title, http.StatusFound)
+}
+
 func main() {
-	http.HandleFunc(routes["view"].path, routes["view"].handler)
-	http.HandleFunc(routes["edit"].path, routes["edit"].handler)
-	//http.HandleFunc("/save/", saveHandler)
+	http.HandleFunc(routes["view"], viewHandler)
+	http.HandleFunc(routes["edit"], editHandler)
+	http.HandleFunc(routes["save"], saveHandler)
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 
